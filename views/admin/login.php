1533a5f0d943b61255e338e8fcdfc5da92d6f3e5
RaulAlvaradoT <ingraulalvarado@hotmail.com>
2023-03-26 14:46:17 -0600

    Se agrega CAPTCHA

--- views/admin/login.php
+++ views/admin/login.php
@@ -1,3 +1,5 @@
+<script src="https://www.google.com/recaptcha/api.js" async defer></script>
+
 <?php
 include(__DIR__ . '/../../class/class_db/class_db.php');
 include(__DIR__ . '/../../class/class_usuarios/class_usuario_dal.php');
@@ -5,16 +7,27 @@ include(__DIR__ . '/../../class/class_usuarios/class_usuario_dal.php');
 if (isset($_POST['submit'])) {
     $id = $_POST['username'];
     $pass = $_POST['password'];
+    $captcha = $_POST['g-recaptcha-response'];
 
-    $obj_usuario = new catalogo_usuario_dal;
-    $result_exis = $obj_usuario->existe_usuario($id, $pass);
-    if ($result_exis == 1) {
-        session_start();
-        $_SESSION['id'] = $id;
-        header("Location: ../users/index.php");
-        exit();
+    // Verifica el captcha
+    $secretKey = "6LeWhTMlAAAAALIvtIXWd8nDeS31zyHSda8isyMM";
+    $response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret={$secretKey}&response={$captcha}");
+    $response = json_decode($response, true);
+
+    if ($response["success"]) {
+
+        $obj_usuario = new catalogo_usuario_dal;
+        $result_exis = $obj_usuario->existe_usuario($id, $pass);
+        if ($result_exis == 1) {
+            session_start();
+            $_SESSION['id'] = $id;
+            header("Location: ../users/index.php");
+            exit();
+        } else {
+            echo "<script>alert('El usuario o la contraseña son incorrectos')</script>";
+        }
     } else {
-        echo "<script>alert('El usuario o la contraseña son incorrectos')</script>";
+        echo "<script>alert('Por favor, completa el captcha')</script>";
     }
 }
 ?>
@@ -37,6 +50,7 @@ if (isset($_POST['submit'])) {
         <input type="text" name="password" id="password">
 
         <input type="submit" name="submit" value="Enviar">
+        <div class="g-recaptcha" data-sitekey="6LeWhTMlAAAAAGp7lWabk7hmOiLk1B7qAh-3m6s_"></div>
     </form>
 
 </body>
