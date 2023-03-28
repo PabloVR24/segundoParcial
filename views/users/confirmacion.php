71607e9deb34513654242b2ffe75b5f242f6d438
RaulAlvaradoT <ingraulalvarado@hotmail.com>
2023-03-28 02:35:55 -0600

    Cambios en CSS de confirmacion

--- views/users/confirmacion.php
+++ views/users/confirmacion.php
@@ -1,11 +1,14 @@
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+    integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3"
+    crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"
+    integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD"
+    crossorigin="anonymous"></script>
 <link rel="stylesheet" href="../../css/styles2.css">
 <?php
 
 require_once(__DIR__ . '/../../class/class_ticket/class_ticket_dal.php');
 require_once(__DIR__ . '/../../class/class_alumno/class_alumno_dal.php');
-require_once('../../actions/generar_qr.php');
 
 // Recuperar los datos y el mensaje de éxito de la URL
 $exito = $_GET['exito'] ?? false;
@@ -28,7 +31,7 @@ if ($exito) {
         $message = "<style='backgroud-color:green'> Registro Exitoso</style>";
     }
 } else {
-    $message = "<style='color:#441b1b'> Registro ya existente </style>";
+    $message = "<style='color:#e5e5e5'> Registro ya existente </style>";
 }
 
 ?>
@@ -41,15 +44,17 @@ if ($exito) {
         </h1>
 
         <?php
-        print ("<p class = 'h4'> ESTATUS: " . $resultado->getESTATUS()) . "</p>";
-        print ("Fecha: " . $resultado->getFECHA()) . "<br>";
+        print("<p class = 'h4'> ESTATUS: " . $resultado->getESTATUS()) . "</p>";
+        print("<br>");
+        print("Fecha: " . $resultado->getFECHA()) . "<br>";
         print("<br>");
         print("Nombre Completo: " . $res_alumno->getNOMBRE() . " " . $res_alumno->getAPELLIDO_PAT() . " " . $res_alumno->getAPELLIDO_MAT());
         print("<br>");
         print("Numero de Turno " . $resultado->getTURNO());
         print("<br><br><br>");
-        print("Registrado por " . $resultado->getNOMBRE_USUARIO())
-        ?>
+        print("Registrado por " . $resultado->getNOMBRE_USUARIO());
+        print("<br>");
+            ?>
 
         <br>
         <input type="checkbox" id="mostrar_area" name="mostrar_area" onchange="mostrarOcultarArea()">Editar<br>
@@ -60,7 +65,8 @@ if ($exito) {
 
                 <div class="box">
                     <label for="fecha1">Fecha de Cita:</label>
-                    <input type="date" id="fecha1" name="fecha1" value="<?php echo isset($_POST["fecha1"]) ? $_POST["fecha1"] : ""; ?>">
+                    <input type="date" id="fecha1" name="fecha1"
+                        value="<?php echo isset($_POST["fecha1"]) ? $_POST["fecha1"] : ""; ?>">
                     <br>
                     <?php if (!empty($error_fecha)) { ?>
                         <span class="error">
@@ -78,9 +84,11 @@ if ($exito) {
 
         <form action="../../actions/generar_pdf.php" method="POST">
             <input hidden type="text" name="lcurp" id="lcurp" value="<?php echo $lcurp ?>">
+            <?php
+            print("<br>");
+            ?>
             <input type="submit" name="submit" value="Generar PDF">
             <input type="hidden" name="create_id" id="create_id" value="<?php echo $create_id ?>">
-            <input type="submit" name="submit" value="Generar QR">
         </form>
 
 
